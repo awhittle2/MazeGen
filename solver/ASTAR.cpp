39126f36df04933f1e6663e68ca3df04c63758e7
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-10 11:09:16 -0800

    Updated comments and moved

--- solver/ASTAR.cpp
+++ solver/ASTAR.cpp
@@ -9,32 +9,35 @@
 
 using namespace std;
 
-// Struct to hold node information
 struct Node {
-  // Current coords
+  // x and y coordinates of the node
   int x, y;
-  // Distance from start
+  
+  // distance from the starting node
   int distF;
-  // Distance to end
+  
+  // distance to the target node
   int distT;
-  // Previous node
+  
+  // pointer to the previous node in the path
   Node* prev;
 };
 
-// Struct to customize the priority queue to be based on the distance from plus the distance to
 struct Compare {
+  // operator to compare two nodes based on their total distance
   bool operator()(const Node* a, const Node* b) {
     return a->distF + a->distT > b->distF + b->distT;
   }
 };
 
-// Struct to allow for pair keys in the unordered map
 struct hash_pair {
+  // operator to hash a pair of values
   template<class T1, class T2>
   size_t operator()(const pair<T1, T2>& p) const {
     auto hash1 = hash<T1>{}(p.first);
     auto hash2 = hash<T2>{}(p.second);
   
+    // combine the hashes of the two values using XOR
     if(hash1 != hash2) {
       return hash1 ^ hash2;
     }
@@ -42,194 +45,174 @@ struct hash_pair {
   }
 };
 
-// Function to find and output all valid neighbors
+// Returns the neighbors of the given node
 vector<Node*> getNeighbors(vector<vector<int>> maze, unordered_map<pair<int,int>, Node*, hash_pair> m, Node* node) {
   vector<Node*> neighbors;
 
-  // If right neighbor is within bounds, not a wall, and not visited
+  // Check if the neighbor to the right is valid and not already visited
   if((node->x + 1) >= 0 && (node->x + 1 < maze.size()) && maze[node->x+1][node->y] != 1 && m.find({node->x+1, node->y}) == m.end()) {
-    // Create a new node
     Node* newN = new Node();
     newN->x = node->x + 1;
     newN->y = node->y;
-    // Push the node to the vector
     neighbors.push_back(newN);
   }
 
-  // If the left neighbor is within bounds, not a wall, and not visited
+  // Check if the neighbor to the left is valid and not already visited
   if(node->x - 1 >= 0 && node->x - 1 < maze.size() && maze[node->x-1][node->y] != 1 && m.find({node->x-1, node->y}) == m.end()) {
-    // Create a new node
     Node* newN = new Node();
     newN->x = node->x - 1;
     newN->y = node->y;
-    // Push the node to the vector
     neighbors.push_back(newN);
   }
 
-  // If the up neighbor is within bounds, not a wall, and not visited
+  // Check if the neighbor above is valid and not already visited
   if(node->y + 1 >= 0 && node->y + 1 < maze.size() && maze[node->x][node->y+1] != 1 && m.find({node->x, node->y+1}) == m.end()) {
-    // Create a new node
     Node* newN = new Node();
     newN->x = node->x;
     newN->y = node->y + 1;
-    // Push the node to the vector
     neighbors.push_back(newN);
   }
 
-  // If the down neighbor is within bounds, not a wall, and not visited
+  // Check if the neighbor below is valid and not already visited
   if(node->y - 1 >= 0 && node->y - 1 < maze.size() && maze[node->x][node->y-1] != 1 && m.find({node->x, node->y-1}) == m.end()) {
-    // Create a new node
     Node* newN = new Node();
     newN->x = node->x;
     newN->y = node->y - 1;
-    // Push the node to the vector
     neighbors.push_back(newN);
   }
 
-  // Return all neighbors
   return neighbors;
 }
 
-// Function to get the euc distance between two nodes
+// Returns the Euclidean distance between two nodes
 int getDistance(Node *a, Node *b) {
-  // Absolute value of x1 - x2
+  // Calculate the x and y distances between the two nodes
   float distX = abs(a->x - b->x);
-  // Absolute value of y1 - y2
   float distY = abs(a->y - b->y);
-  // Square the first term
+
+  // Square the distances
   distX = pow(distX, 2);
-  // Square the second term
   distY = pow(distY, 2);
 
-  // Add the two terms together
+  // Take the square root of the sum of the squares to get the Euclidean distance
   float result = distX + distY;
-  // Square the result and return it
   result = sqrt(result);
   return result;
 }
 
-// Function to recursively reverse from end node to start node
+// Recursively follow the path of the node back to the starting node
 void recurse(Node* node, stack<Node*>& s, int startX, int startY) {
-  // If there is a previous node
+  // If the current node has a previous node in the path, continue recursion
   if(node->prev) {
-    // Push it to the stack
     s.push(node);
-    // Try again with the previous previous node
     recurse(node->prev, s, startX, startY);
-    // If there is no previous node
-  } else {
-    // Push the starting node to the stack
+  }
+  // Otherwise, the starting node has been reached
+  else {
+    // Create a new node representing the starting node
     Node *temp = new Node();
     temp->x = startX;
     temp->y = startY;
     s.push(temp);
 
-    // Outputs to txt file
+    // Open the output file and print the path in the specified format
     freopen("output.txt","w",stdout);
 
     cout << "{";
     while(!s.empty()) {
-      // Output the top of the stack
       cout << "{" << s.top()->x << "," << s.top()->y << "}" << (s.size() - 1 == 0 ? "" : ",");
-      // Pop it off of the stack
       s.pop();
     }
     cout << "}";
     fclose(stdout);
+
+    // End the function
     return;
   }
 }
 
-// Function to set up and perform a* seach
+// Solves the maze using the A* search algorithm
 void aStarMazeSolver(vector<vector<int>> maze, int startX, int startY) {
-  // Node start info
+  // Create the starting node
   Node* source = new Node();
   source->x = startX;
   source->y = startY;
   source->distF = 0;
 
+  // Create the goal node
   Node* goal = new Node();
 
-  // Search for the ending node
+  // Find the location of the goal node in the maze
   for(int i = 0; i < maze.size(); i++) {
     for(int j = 0; j < maze[0].size(); j++) {
-      // If goal node
       if(maze[i][j] == 2) {
-        // Update the goal node info
         goal->x = i;
         goal->y = j;
-        // Set the distanceTo 0
         goal->distT = 0;
-        // Set the distanceFrom to be the euc distance
         goal->distF = getDistance(goal, source);
       }
     }
   }
 
-  // Set the starting distanceTo to be the euc distance
+  // Calculate the distance from the starting node to the goal node
   source->distT = getDistance(source, goal);
 
-  // Priority queue based on minimum distances from plus the minimum distances to of the nodes
+  // Create a priority queue and a hash map to store the nodes
   priority_queue<Node*, vector<Node*>, Compare> q;
-  // Unordered map for visited values
   unordered_map<pair<int,int>, Node*, hash_pair> m;
 
-  // Push the starting node to the pq
+  // Add the starting node to the queue
   q.push(source);
 
+  // Flag to keep track of when the goal node has been found
   bool found = false;
+  // Continuously search for the goal node
   while(!found) {
-    // Explore and pop the top node
+    // Get the top node from the queue
     Node* node = q.top();
     q.pop();
 
-    // Current node info
+    // Store the coordinates and distances of the node
     int x = node->x;
     int y = node->y;
     int distanceT = node->distT;
     int distanceF = node->distF;
 
-    // Add to visited
+    // Add the node to the hash map
     m[{x,y}] = node;
 
-    // If current is the goal
+    // If the current node is the goal node, set the found flag to true
     if(maze[x][y] == 2) {
-      // Exit out of the while loop
       found = true;
     }
 
-    // Find all valid neighbors
+    // Get the neighbors of the current node
     vector<Node*> neighbors = getNeighbors(maze,m,node);
 
-    // Go through each neighbor
+    // Loop through the neighbors and update their distances and previous nodes
     for(int i = 0; i < neighbors.size(); i++) {
-      // Create a new node at the index
       Node* current = neighbors[i];
-      // Distance to is the euc distance
       current->distT = getDistance(current,goal);
-      // Distance from is current distance + 1
       current->distF = node->distF + 1;
-      // Previous node is current
       current->prev = node;
 
-      // Push the node to the priority queue
+      // Add the updated node to the queue
       q.push(current);
     }
   }
 
-  // Loop through all items in the unordered map
+  // Loop through the hash map to find the goal node
   for(auto it : m) {
+    // If the current node is the goal node, print the path to the output file
     if(maze[it.first.first][it.first.second] == 2) {
-      // Create a stack as temp storage for reversing
       stack<Node*> s;
-      // Recursively reverse until starting node
       recurse(it.second, s, startX, startY);
     }  
   }
 }
 
 int main() {
-  // Maze input
+  // Create the maze as a 2D vector
   vector<vector<int>> maze = {
 	{1,0,1,0,0,0,0,0,0,0},
 	{0,0,0,0,1,0,1,0,1,1},
@@ -242,11 +225,12 @@ int main() {
 	{0,0,0,1,0,1,0,1,2,0},
 	{0,1,0,0,0,0,1,1,1,0}
   };
-  // Pacman starting position
+
+  // Store the starting position
   int startY = 0, startX = 7;
 
+  // Solve the maze using the A* search algorithm
   aStarMazeSolver(maze, startY, startX);
 
-  // Program close
   return 0;
 }
