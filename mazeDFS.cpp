cf1546fe892a9d553a1c65edc61f8d479e061f73
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-09 23:33:17 -0800

    Added comments

--- mazeDFS.cpp
+++ mazeDFS.cpp
@@ -8,136 +8,139 @@
 
 using namespace std;
 
-// Function to find the number of non wall valid neighbors
+// This function counts the number of walls surrounding a given coordinate (x, y) in a maze.
 int findNeighbors(vector<vector<int>> maze, int x, int y) {
   int count = 0;
-  // Valid directions
+  // These are the four possible directions in which the function can check for walls.
   vector<pair<int,int>> directions = {{1,0},{0,1},{-1,0},{0,-1}};
 
-  // Goes through each direction
   for(int i = 0; i < 4; i++) {
+    // nx and ny are the coordinates of the adjacent cell in the direction specified by the current value of i.
     int nx = x + directions[i].first;
     int ny = y + directions[i].second;
 
-    // If out of bounds, go to the next neighbor
+    // If the coordinates are outside the bounds of the maze, skip them.
     if(nx < 0 || ny < 0 || nx >= maze.size() || ny >= maze[0].size()) continue;
 
-    // If not a wall
-    if(maze[nx][ny] == 0) {
-      // Increase the count
+    // If there is a wall at the adjacent cell, increment the count.
+    if(maze[nx][nx] == 0) {
       count = count + 1;
     }
   }
+  // Return the number of walls surrounding the given coordinate.
   return count;
 }
 
-// Function to recursively preform dfs to create a maze
+// This function is used to generate the maze using the depth-first search algorithm.
 void dfsMaze(vector<vector<int>>& maze, vector<vector<bool>> visited, vector<pair<int,int>> neighbors, unsigned seed, int x,int y) {
-  // If out of bounds, visited, or not a wall return
+  // If the current coordinates are outside the bounds of the maze, or if the current cell has already been visited, or if the current cell is a wall, return.
   if(x < 0 || x >= maze.size() || y < 0 || y >= maze[0].size() || visited[x][y] || maze[x][y] == 0) return;
-
-  // Mark as visited
+  // Mark the current cell as visited.
   visited[x][y] = true;
+  // Count the number of walls surrounding the current cell.
   int count = 0;
-  // Finds the number of nonwall neighbors
   count = findNeighbors(maze, x, y);
 
-  // If there is 2 or more nonwall neighbors return
+  // If the number of walls surrounding the current cell is greater than 1, return.
   if(count > 1) return;
 
-  // Make the current coordinates not a wall
+  // Set the current cell to a path.
   maze[x][y] = 0;
 
-  // Shuffles the directions
+  // Shuffle the list of possible directions.
   shuffle(neighbors.begin(), neighbors.end(), default_random_engine(seed));
 
-  // Goes to the first direction
+  // Recursively generate the maze in each of the possible directions.
   dfsMaze(maze, visited, neighbors, seed, x+neighbors[0].first, y+neighbors[0].second);
-  // Goes to the second direction
   dfsMaze(maze, visited, neighbors, seed, x+neighbors[1].first, y+neighbors[1].second);
-  // Goes to the third direction
   dfsMaze(maze, visited, neighbors, seed, x+neighbors[2].first, y+neighbors[2].second);
-  // Goes to the fourth direction
   dfsMaze(maze, visited, neighbors, seed, x+neighbors[3].first, y+neighbors[3].second);
 }
 
-// Function to set the goal in the maze
 void setGoal(vector<vector<int>>& maze) {
   bool isSet = false;
+  // Keep generating random coordinates until a valid location for the goal is found.
   while(!isSet) {
-    // Random x
     int x = rand() % maze.size();
-    // Random y
     int y = rand() % maze[0].size();
 
-    // If the coords is not a wall
+    // If the current cell is not a wall, set it as the goal and end the loop.
     if(maze[x][y] != 1) {
-      // Set it to the goal
       maze[x][y] = 2;
       isSet = true;
     }
   }
 }
 
-// Function to set the starting position for pacman
 pair<int,int> setPacMan(vector<vector<int>> maze) {
   bool isSet = false;
+  // Keep generating random coordinates until a valid location for Pac-Man is found.
   while(!isSet) {
-    // Random x
     int x = rand() % maze.size();
-    // Random y
     int y = rand() % maze[0].size();
 
-    // If the coords is not a wall or the goal
+    // If the current cell is not a wall or the goal, set it as the starting location for Pac-Man and return the coordinates.
     if(maze[x][y] != 1 && maze[x][y] != 2) {
       isSet = true;
-      // Set starting to that position
       return {x,y};
     }
   }
+  // If no valid starting location for Pac-Man was found, return an empty pair of coordinates.
   return {};
 }
 
-// Function to create vectors, call the recursive function, and set the goal and starting position
 void dfsMazeGenerator(vector<vector<int>>& maze) {
-  // Vector to hold visited
+
+  // Create a 2D vector to keep track of which cells have been visited during the maze generation.
   vector<vector<bool>> visited(maze.size(), vector<bool>(maze[0].size(), false));
-  // Vector to hold possible directions of the neighbors
+
+  // These are the four possible directions in which the maze generation can proceed.
   vector<pair<int,int>> neighbors = {{1,0},{0,1},{-1,0},{0,-1}};
-  // Allows for random maze generation
+ 
+  // Use the current time as a seed for the random number generator.
   unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 
+  // Start generating the maze at the middle of the grid.
   dfsMaze(maze, visited, neighbors, seed, ceil(maze.size()/2), ceil(maze[0].size()/2));
 
-  // Sets the goal and adds it to the maze
+  // Set the goal in the maze.
   setGoal(maze);
-  // Finds the starting position
+
+  // Set the starting location for Pac-Man in the maze.
   pair<int,int> starting = setPacMan(maze);
-  // Oututs to txt file
+
+  // Open the output file for writing and print the starting position of Pac-Man.
   freopen("output.txt","w",stdout);
   cout << "Pacman's starting position: X : " << starting.first << "   Y: " << starting.second << "\n\n";
   fclose(stdout);
 }
 
 int main() {
-  // Size of maze (nxn)
+  // Set the size of the maze.
   int n = 10;
-  // Vector to hold maze, initialized to be all walls
-  vector<vector<int>> maze(n, vector<int>(n,1)); // Initialize vector of size n by n full of walls
+  // Initialize the maze with walls.
+  vector<vector<int>> maze(n, vector<int>(n,1));
 
+  // Generate the maze.
   dfsMazeGenerator(maze);
 
-  // Outputs the entire maze
-  // Outputs to txt file
-  freopen("output.txt","w",stdout);
+// Open the output file for writing and print the opening bracket for the maze representation.
+freopen("output.txt","w",stdout);
+cout << "{";
+
+// Loop through each row in the maze.
+for(int i = 0; i < maze.size(); i++) {
+  // Print the opening bracket for the current row.
   cout << "{";
-  for(int i = 0; i < maze.size(); i++) {
-    cout << "{";
-    for(int j = 0; j < maze.size(); j++) {
-      cout << maze[i][j] << (j == maze[0].size() - 1 ? "" : ",");
-    }
-    cout << "}" << (i == maze.size() - 1 ? "" : ",") << "\n";
+  // Loop through each cell in the current row.
+  for(int j = 0; j < maze.size(); j++) {
+    // Print the value of the current cell. If it is the last cell in the row, don't print a comma after it.
+    cout << maze[i][j] << (j == maze[0].size() - 1 ? "" : ",");
   }
-  cout << "}";
-  fclose(stdout);
+  // Print the closing bracket for the current row. If it is the last row, don't print a comma after it.
+  cout << "}" << (i == maze.size() - 1 ? "" : ",") << "\n";
+}
+// Print the closing bracket for the maze representation and close the output file.
+cout << "}";
+fclose(stdout);
 }
